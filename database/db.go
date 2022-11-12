ebe7e8f121ea8c89515a61ab66857b26da0e72bf
Your Name <you@example.com>
2022-11-12 18:43:00 -0300

    DB variables

--- database/db.go
+++ database/db.go
@@ -15,7 +15,7 @@ var (
 
 func ConectaComBancoDeDados() {
 	
-	stringDeConexao := "host="+os.GetEnv("HOST")+" user="+os.GetEnv("USER")+" password="+os.GetEnv("PASSWORD")+" dbname="+os.GetEnv("DBNAME")+" port="+os.GetEnv("PORT")+" sslmode=disable"
+	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("PORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")

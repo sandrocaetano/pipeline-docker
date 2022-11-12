9d22bf030fb5c37711659b968d2afe1740b87a7e
Your Name <you@example.com>
2022-11-12 18:38:21 -0300

    first commit

--- database/db.go
+++ database/db.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"log"
-
+    "os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,7 +14,8 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	
+	stringDeConexao := "host="+os.GetEnv("HOST")+" user="+os.GetEnv("USER")+" password="+os.GetEnv("PASSWORD")+" dbname="+os.GetEnv("DBNAME")+" port="+os.GetEnv("PORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
